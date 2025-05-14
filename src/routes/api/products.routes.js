b6afe8c03c14dcb3b40e217af8a2bc2b2b29d07a
Luis Fernando Rodriguez Santamaria <luisfernandorodriguezsantamaria@Luiss-MacBook-Air.local>
2025-05-14 19:00:59 +0200

    Ultimo arreglo de get todo status

--- src/routes/api/products.routes.js
+++ src/routes/api/products.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { checkToken } = require('../../utils/middleware');
-const { getProducts, getProductById, createProduct, updateProduct, deleteProductById, getProductInventoryStatus, getProductByBarcode } = require('../../controllers/products.controllers');
+const { getProducts, getProductById, createProduct, updateProduct, deleteProductById, getProductInventoryStatus, getProductByBarcode, getAllProductsInventoryStatus } = require('../../controllers/products.controllers');
 
 router.get('/', checkToken, getProducts);
+router.get('/product-status', checkToken, getAllProductsInventoryStatus);
 router.get('/product-status/:id', checkToken, getProductInventoryStatus); // Esta ruta debe ir ANTES de '/:id' ojooooo
 router.get('/barcode/:code', checkToken, getProductByBarcode);
 router.get('/:id', checkToken, getProductById);
