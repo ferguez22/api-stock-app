30e9bfe4cbf0dadebc3eac78200f825d30908946
Fernando <98433286+ferguez22@users.noreply.github.com>
2026-03-26 16:20:56 +0100

    Retoques que quedaban

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11,22 +11,3 @@ const createToken = (user) => {
 };
 
 module.exports = { createToken };
-
-
-
-
-// const jwt = require('jsonwebtoken');
-// const SECRET_KEY = 'xxxxxx';   
-
-// const createToken = (user) => {
-//     const data = {
-//         usuario_id: user._id,
-//         usuario_role: user.role
-//     }
-//     return jwt.sign(data, SECRET_KEY)
-// }
-
-// module.exports = {
-//     createToken,
-//     SECRET_KEY
-// }   
